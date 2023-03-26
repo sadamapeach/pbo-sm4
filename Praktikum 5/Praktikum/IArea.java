31291b25d4c93749920690d17e2507636c8385c0
sadamapeach <100354765+sadamapeach@users.noreply.github.com>
2023-03-26 13:15:05 +0700

    Update IArea.java

--- Praktikum 5/Praktikum/IArea.java
+++ Praktikum 5/Praktikum/IArea.java
@@ -3,10 +3,9 @@
 * Tanggal	: 26 Maret 2023
 * Penulis	: Oktaviana Sadama Nur Azizah
 * NIM		: 24060121130060
-* Deskripsi	: Interface yang mendefinisikan kontrak bahwa
-			  setiap kelas yang membuat implementasi dari
-			  interface ini harus memiliki cara untuk 
-			  menghitung luas
+* Deskripsi	: Interface yang mendefinisikan kontrak bahwa setiap kelas 
+		  yang membuat implementasi dariinterface ini harus memiliki 
+		  cara untuk menghitung luas
 **/
 
 public interface IArea {
