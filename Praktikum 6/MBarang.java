0250faec4012a263e6712c138ed03dd637262bcd
sadamapeach <100354765+sadamapeach@users.noreply.github.com>
2023-04-07 10:57:17 +0700

    Update MBarang.java

--- Praktikum 6/MBarang.java
+++ Praktikum 6/MBarang.java
@@ -9,10 +9,10 @@
 public class MBarang {
     public static void beliBarang(Barang barang, int jumlah) throws StokException {
         if (jumlah > barang.getStok()) {
-			throw new StokException(barang);            
+	    throw new StokException(barang);            
         } else {
             System.out.println("Anda berhasil membeli "+jumlah+" "+barang.getNama());
-			barang.kurangiStok(jumlah);
+	    barang.kurangiStok(jumlah);
         }
     }
     
@@ -20,11 +20,11 @@ public class MBarang {
         Barang barang = new Barang("B003", "Indomie", 25);
         try {
             beliBarang(barang, 15);
-			beliBarang(barang, 10);
-			beliBarang(barang, 5);
+	    beliBarang(barang, 10);
+	    beliBarang(barang, 5);
         } catch (StokException e) {
             System.out.println(e.getMessage());
-			System.out.println("Mohon maaf, stok barang tidak mencukupi.");
+	    System.out.println("Mohon maaf, stok barang tidak mencukupi.");
         }
     }
 
